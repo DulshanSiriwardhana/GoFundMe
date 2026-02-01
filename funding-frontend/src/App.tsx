2b99997765e44fe75a15c1b0ea3e2b9ef3dff891
Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2026-02-01 09:22:14 +0530

    finalized the UI

--- funding-frontend/src/App.tsx
+++ funding-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Web3Provider } from "./context/Web3Context";
 import { AlertProvider } from "./context/AlertContext";
+import { PageTransitionProvider } from "./context/PageTransitionContext";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import CreateFund from "./pages/CreateFund";
@@ -13,15 +14,17 @@ export default function App() {
     <Web3Provider>
       <AlertProvider>
         <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/create" element={<CreateFund />} />
-              <Route path="/my-funds" element={<MyFunds />} />
-              <Route path="/fund/:address" element={<FundDetails />} />
-              <Route path="/admin" element={<AdminDashboard />} />
-            </Routes>
-          </Layout>
+          <PageTransitionProvider>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/create" element={<CreateFund />} />
+                <Route path="/my-funds" element={<MyFunds />} />
+                <Route path="/fund/:address" element={<FundDetails />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+              </Routes>
+            </Layout>
+          </PageTransitionProvider>
         </BrowserRouter>
       </AlertProvider>
     </Web3Provider>
--- funding-frontend/src/context/PageTransitionContext.tsx
+++ funding-frontend/src/context/PageTransitionContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import { createContext, useContext, useState, useEffect, type ReactNode } from 'react';
 import { useLocation } from 'react-router-dom';
 import Loader from '../components/Loader';
 
